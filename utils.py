8266224a727479e6a07a2cb8af45c60da68d75a2
Alexander Green <aeg0322@gmail.com>
2015-07-27 20:41:54 -0400

    save

--- utils.py
+++ utils.py
@@ -1,15 +1,7 @@
-"""thIS iS a veRy speCIAL modULE thAT cAN heLP yOU dO stuFF."""
+"My utils module"
 
-import colors as c
-
-def ask(question,color=c.yellow):
-    print(color + question + c.reset)
-    answer = input(c.green + "> " + c.red).lower().strip()
+def ask(question):
+    print(question)
+    answer = input(c.green + '> ' + c.red).lower()
     print(c.reset)
     return answer
-
-
-if __name__ == '__main__':
-    print(c.clear)
-    name = ask("What is your name?")
-    color = ask("What is your name in color?",c.random_color())
